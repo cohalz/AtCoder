f0bf12ef9306cd310b1d0b9042d6e15f37a9867c
cohalz <cohalzweb@gmail.com>
2014-03-13 13:13:03 +0900

    add ARC13-a

--- ARC/13/a.scala
+++ ARC/13/a.scala
@@ -11,13 +11,18 @@ object Main {
     var tmp1 = for(i <- 1 to n if i%p == 0) yield i
     var tmp2 = for(i <- 1 to m if i%q == 0) yield i
     var tmp3 = for(i <- 1 to l if i%r == 0) yield i
-    val count1 = tmp1.length * tmp2.length * tmp3.length
     var tmp4 = for(i <- 1 to n if i%q == 0) yield i
-    var tmp5 = for(i <- 1 to m if i%p == 0) yield i
-    val count2 = tmp4.length * tmp5.length * tmp3.length
-    if(tmp3 contains l) println(count1 max count2)
-    else if(!(tmp1.contains(n)) && !(tmp2.contains(m)) && tmp4.contains(n) && tmp5.contains(m)) println(count2)
-    else if(!(tmp4.contains(n)) && !(tmp5.contains(m)) && tmp1.contains(n) && tmp2.contains(m)) println(count1)
-    else println(0)
+    var tmp5 = for(i <- 1 to m if i%r == 0) yield i
+    var tmp6 = for(i <- 1 to l if i%p == 0) yield i
+    var tmp7 = for(i <- 1 to n if i%r == 0) yield i
+    var tmp8 = for(i <- 1 to m if i%p == 0) yield i
+    var tmp9 = for(i <- 1 to l if i%q == 0) yield i
+    var c1 = tmp1.length * tmp2.length * tmp3.length
+    var c2 = tmp1.length * tmp5.length * tmp9.length
+    var c3 = tmp4.length * tmp5.length * tmp6.length
+    var c4 = tmp4.length * tmp8.length * tmp3.length
+    var c5 = tmp7.length * tmp8.length * tmp9.length
+    var c6 = tmp7.length * tmp2.length * tmp6.length
+    println(c1 max c2 max c3 max c4 max c5 max c6)
   }
 }
